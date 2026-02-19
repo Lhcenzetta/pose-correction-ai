718daadd9f180a1b614c96dc8ff71cfa9d6356da
lait-zet <lhcenztta18@gmail.com>
2026-02-19 22:40:51 +0000

    restructur the branch

--- PiplineCNN/src/manage_live.py
+++ PiplineCNN/src/manage_live.py
@@ -1,17 +1,17 @@
 import cv2
+import os
+from dotenv import load_dotenv
 import numpy as np
 from tensorflow.keras.models import load_model
 from collections import deque
+load_dotenv()
 
+model_path = os.getenv("model_path")
 
-
-
-# Load trained model
-model = load_model("/Users/lait-zet/Desktop/pose-correction-ai/model/pose_correction_model.h5")
+model = load_model(model_path)
 
 IMG_SIZE = 128
 
-# Open webcam
 cap = cv2.VideoCapture(0)
 
 correct_frames = 0
@@ -23,18 +23,16 @@ while True:
     if not ret:
         break
 
-    # Resize for model
     img = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # Predict
+
     prediction = model.predict(img, verbose=0)
     probability = prediction[0][0]
 
     prediction_buffer.append(probability)
 
-    # Average last predictions
     smoothed_prob = np.mean(prediction_buffer)
 
     total_frames += 1
@@ -46,8 +44,7 @@ while True:
     else:
         label = "INCORRECT"
         color = (0, 0, 255) 
-
-    # Display text
+        
     cv2.putText(frame, f"{label} ({probability:.2f})",
                 (20, 40),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -56,15 +53,14 @@ while True:
                 2)
 
     cv2.imshow("Pose Correction AI", frame)
-
-    # Press Q to quit
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
 
-# Final Score
+
 if total_frames > 0:
     score = (correct_frames / total_frames) * 100
     if score >= 80:
